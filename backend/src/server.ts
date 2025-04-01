3d16ba9a8c623ef604c0be82d5e561852198e1a6
Haris Mališević <malisevic.haris@gmail.com>
2025-04-01 22:02:44 +0200

    API routes for documentType and controller defined and respond to request

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,15 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
 import path from 'path';
+import documentTypeRoutes from './routes/documentType.routes';
+
+// Load environment variables
+dotenv.config();
 
 const APP = express();
 const PORT = 5000;
+
+APP.use(express.json());
 
 // Define the path to the frontend build folder
 const FRONTEND_BUILD_PATH = path.join(__dirname, "../../frontend/build");
@@ -18,6 +25,12 @@ APP.get("/", (req, res) => {
 APP.get("/api/message", (req, res) => {
     res.json({ message: "Hello from backend!" });
 });
+
+// API Routes
+APP.use("/document-types", documentTypeRoutes);
+
+
 APP.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
