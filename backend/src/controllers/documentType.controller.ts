e4aa8eb562a1411e14637ae26b9a6bc96f50e6b1
Haris Mališević <malisevic.haris@gmail.com>
2025-04-04 01:26:43 +0200

    Routes GET:/document-type and POST:/document-type. POST needs user auth before accepting input

--- backend/src/controllers/documentType.controller.ts
+++ backend/src/controllers/documentType.controller.ts
@@ -1,14 +1,44 @@
 import { Request, Response } from "express";
+import db from "../database/db";
 
 class DocumentTypeController {
-  static getAll(req: Request, res: Response) {
+  static async getAll(req: Request, res: Response): Promise<void> {
     // Fetch document types from database
-    res.json({ message: "List of document types" });
+
+    const documentTypes = await db.document_types.findAll();
+
+    res.json(documentTypes);
   }
 
-  static create(req: Request, res: Response) {
+  static async create(req: Request, res: Response): Promise<void> {
     // Add a new document type to the database
-    res.status(201).json({ message: "Document type added" });
+
+    // TODO: Authentication check
+
+    interface DocumentTypeRequest {
+      name: string;
+      description?: string;
+      created_by: number;
+    }
+
+    const jsonReq: DocumentTypeRequest = req.body || {};
+
+    if (!jsonReq.name) {
+      res.status(400).json({ message: "Name is required" });
+      return;
+    }
+    // if (!created_by) { return res.status(400).json({ message: "Document type author ID is required" }); }
+
+    try {
+      await db.document_types.create({
+        name: jsonReq.name,
+        description: jsonReq.description,
+        created_by: jsonReq.created_by,
+      });
+      res.status(200).json({ message: "Document type added successfully" });
+    } catch (error) {
+      res.status(400).json({ message: "Failed to add document type", error });
+    }
   }
 
   static remove(req: Request, res: Response) {
