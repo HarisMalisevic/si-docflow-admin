841a65cfd8b2210ea95390bc9fbd7ebbcd75adb2
Haris Mališević <malisevic.haris@gmail.com>
2025-04-04 01:23:28 +0200

    Removed type definition folder, type definitions can go with DB models for consistency

--- backend/src/types/DocumentType.ts
+++
@@ -1,7 +0,0 @@
-export type DocumentType = {
-    id: string;
-    name: string;
-    description: string;
-    createdBy: string; // Reference to the creator
-    createdAt: Date; // Timestamp of creation
-};
