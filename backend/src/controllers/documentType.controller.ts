ade2f32148342375046083d6a9efac34b7d505f6
Haris Mališević <malisevic.haris@gmail.com>
2025-04-06 23:46:48 +0200

    Logging which user created a document type

--- backend/src/controllers/documentType.controller.ts
+++ backend/src/controllers/documentType.controller.ts
@@ -19,27 +19,20 @@ class DocumentTypeController {
   static async create(req: Request, res: Response): Promise<void> {
     // Add a new document type to the database
 
-    // TODO: Authentication check
-
-    interface DocumentTypeRequest {
-      name: string;
-      description?: string;
-      created_by: number;
-    }
-
-    const jsonReq: DocumentTypeRequest = req.body || {};
+    const jsonReq: { name: string; description?: string; } = req.body || {};
 
     if (!jsonReq.name) {
       res.status(400).json({ message: "Name is required" });
       return;
     }
-    // if (!created_by) { return res.status(400).json({ message: "Document type author ID is required" }); }
+
+    const userID: number = (req.user as { id: number }).id;
 
     try {
       await db.document_types.create({
         name: jsonReq.name,
         description: jsonReq.description,
-        created_by: jsonReq.created_by,
+        created_by: userID
       });
       res.status(200).json({ message: "Document type added successfully" });
     } catch (error) {
@@ -50,8 +43,6 @@ class DocumentTypeController {
   static async remove(req: Request, res: Response): Promise<void> {
     const { id } = req.params;
     // Remove the document type from the database
-
-    //TODO: Authentication check
 
     try {
       const documentType = await db.document_types.findOne({ where: { id } });
