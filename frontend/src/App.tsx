68e4b2143f616267b17c3c6ac5192452ad90c5af
BenjaminHadzihasanovic <bhadzihasa1@etf.unsa.ba>
2025-04-07 02:56:10 +0200

    Added HomePage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route } from "react-router";
-
+import HomePage from "./components/HomePage";
 import DocumentTypeViewer from "./components/DocumentTypeViewer";
 import AppNavbar from "./components/Navbar";
 
@@ -10,7 +10,7 @@ function App() {
     <div>
       <AppNavbar />
       <Routes>
-        <Route path="/" element={<div></div>} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/document-types" element={<DocumentTypeViewer />} />
         {/* Add more routes as needed */}
       </Routes>
