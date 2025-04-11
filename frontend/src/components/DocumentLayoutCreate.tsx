0d778cc7994e6d157e862576b602886bdac92af0
Esma Kurtovic <ekurtovic4@etf.unsa.ba>
2025-04-11 20:27:39 +0200

    Added buttons and field data input form

--- frontend/src/components/DocumentLayoutCreate.tsx
+++ frontend/src/components/DocumentLayoutCreate.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import { Stage, Layer, Image } from "react-konva";
 import Annotation, { AnnotationProps } from "./Annotation";
 import { ShapeProps } from "./Annotation";
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 
 function DocumentLayoutCreate() {
     const [annotations, setAnnotations] = useState<AnnotationProps[]>([]);
     const [newAnnotation, setNewAnnotation] = useState<AnnotationProps[]>([]);
+    const [fieldName, setFieldName] = useState("");
 
     type CanvasMeasures = {
         width: number;
@@ -14,7 +15,7 @@ function DocumentLayoutCreate() {
     };
 
     const [canvasMeasures, setCanvasMeasures] = useState<CanvasMeasures>({
-        width: window.innerWidth,
+        width: window.innerWidth / 2,
         height: window.innerHeight,
     });
 
@@ -53,78 +54,193 @@ function DocumentLayoutCreate() {
 
     const saveAnnotation = () => {
       const updatedAnnotations = [...annotations];
-      updatedAnnotations[updatedAnnotations.length - 1].shapeProps.stroke = "blue";
-      updatedAnnotations[updatedAnnotations.length - 1].saved = true;
-      
+      updatedAnnotations[updatedAnnotations.length - 1] = {
+        ...updatedAnnotations[updatedAnnotations.length - 1],
+        shapeProps: {
+          ...updatedAnnotations[updatedAnnotations.length - 1].shapeProps,
+          stroke: "blue",
+        },
+        saved: true,
+        name: fieldName,
+      };
+
+      setFieldName("");
       setAnnotations(updatedAnnotations);
+    }
+
+    const clearAll = () => {
+      setAnnotations([]);
+    }
+
+    const saveLayout = async () => {
+      try {
+        if(annotations.length > 0 && !annotations[annotations.length - 1].saved) {  //remove the last drawn annotation if not saved
+          annotations.pop();
+        }
+
+        let annotationsFields = annotations.map((annotation) => {
+          return {
+            name: annotation.name,
+            upper_left: [annotation.shapeProps.x, annotation.shapeProps.y],
+            lower_right: [annotation.shapeProps.x + annotation.shapeProps.width, annotation.shapeProps.y + annotation.shapeProps.height],
+          };
+        });
+
+        let fields = JSON.stringify(annotationsFields);   //stringified annotations
+
+        console.log(annotationsFields);
+        console.log(fields);
+
+        //for testing purposes, should only be in the if(response.ok) block
+        setImage(null);
+        setAnnotations([]);
+        setNewAnnotation([]);
+        setCanvasMeasures({
+          width: window.innerWidth / 2,
+          height: window.innerHeight,
+        });
+        //
+
+        /*const response = await fetch("/api/document-layouts", {   //should be this route
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+              //name - user should input before saving the layout
+              //fields - done
+              //document_type - user should choose when uploading a file
+              //image_width, image_height - canvas measures
+          }),
+        });
+
+        if(response.ok) {
+          //remove the uploaded image and previous annotations, reset canvas
+          setImage(null);
+          setAnnotations([]);
+          setNewAnnotation([]);
+          setCanvasMeasures({
+            width: window.innerWidth / 2,
+            height: window.innerHeight,
+          });
+        } 
+        else {
+          console.error("Failed to save layout");
+        }*/
+      } catch (error) {
+        console.error("Error while saving layout: ", error);
+      }
+    }
+
+    const handleFileUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const fileInput = e.target as HTMLInputElement;
+      const file = fileInput.files?.[0];
+      if (!file) return;
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        const img = new window.Image();
+        img.src = reader.result as string;  //the result is available via reader.result after reader.readAsDataURL is done
+        img.onload = () => {
+          const maxWidth = window.innerWidth / 2;
+          const scale = Math.min(maxWidth / img.width, 1);
+
+          setImage(img);
+          setCanvasMeasures({             //scale the canvas (and the image)
+            width: img.width * scale, 
+            height: img.height * scale 
+          });
+          setAnnotations([]);
+          setNewAnnotation([]);
+        }
+      };
+      reader.readAsDataURL(file); //tells the FileReader to start reading the file you selected
+                                  //and convert it to Base64-encoded URL string (data URL)
+      fileInput.value = "";
     }
     
     const annotationsToDraw = [...annotations, ...newAnnotation];
     
     return (
-        <div tabIndex={1}>
-          <input
-            type="file"
-            accept="image/*"
-            onChange={(e) => {
-              const file = e.target.files?.[0];
-              if (!file) return;
-
-              const reader = new FileReader();
-              reader.onload = () => {
-                const img = new window.Image();
-                img.src = reader.result as string;  //the result is available via reader.result after reader.readAsDataURL is done
-                img.onload = () => {
-                  const maxWidth = window.innerWidth / 2;
-                  const scale = Math.min(maxWidth / img.width, 1);
-
-                  setImage(img);
-                  setCanvasMeasures({             //scale the canvas (and the image)
-                    width: img.width * scale, 
-                    height: img.height * scale 
-                  });
-                  setAnnotations([]);
-                  setNewAnnotation([]);
-                }
-              };
-              reader.readAsDataURL(file); //tells the FileReader to start reading the file you selected
-                                          //and convert it to Base64-encoded URL string (data URL)
-            }}
-          />
-
-          <Stage
-            width={canvasMeasures.width}
-            height={canvasMeasures.height}
-            onMouseDown={handleMouseDown}
-            onMouseMove={handleMouseMove}
-            onMouseUp={handleMouseUp}
-          >
-            <Layer>
-              {image && (
-                <Image
-                  image={image}
-                  x={0} // Set image position on canvas
-                  y={0}
-                  width={canvasMeasures.width} // Adjust image size to fill the canvas
-                  height={canvasMeasures.height}
-                />
-              )}
-              {annotationsToDraw.map((annotation, i) => {
-                return (
-                  <Annotation
-                    key={i}
-                    {...annotation}
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <div id="fileDisplay">
+            <Stage
+              width={canvasMeasures.width}
+              height={canvasMeasures.height}
+              onMouseDown={handleMouseDown}
+              onMouseMove={handleMouseMove}
+              onMouseUp={handleMouseUp}
+            >
+              <Layer>
+                {image && (
+                  <Image
+                    image={image}
+                    x={0} // Set image position on canvas
+                    y={0}
+                    width={canvasMeasures.width} // Adjust image size to fill the canvas
+                    height={canvasMeasures.height}
                   />
-                );
-              })}
-            </Layer>
-          </Stage>
-          <Button
-            variant="light"
-            onClick={saveAnnotation}
-          >
-            Save
-          </Button>
+                )}
+                {annotationsToDraw.map((annotation, i) => {
+                  return (
+                    <Annotation
+                      key={i}
+                      {...annotation}
+                    />
+                  );
+                })}
+              </Layer>
+            </Stage>
+          </div>         
+
+          <div id="buttonsAndForms">
+            <div id="buttons" style={{ marginBottom: "30px", display: "flex", flexDirection: "row" }}>
+              <Form.Group controlId="file-upload">
+                <Form.Label htmlFor="file-upload-input" style={{ cursor: "pointer" }}>
+                  <Button as="span" variant="primary">Upload Image</Button>
+                </Form.Label>
+
+                <Form.Control
+                  id="file-upload-input"
+                  type="file"
+                  accept="image/*"
+                  style={{ display: "none" }}
+                  onChange={ handleFileUpload }
+                />
+              </Form.Group>
+
+              <Button
+                variant="primary"
+                onClick={ saveLayout }
+              >
+                Save Layout
+              </Button>
+
+              <Button
+                variant="danger"
+                onClick={ clearAll }
+              >
+                Clear All
+              </Button>
+            </div>
+            
+            <h4>Field Properties</h4>
+            <Form>
+              <Form.Group controlId="fieldName">
+                <Form.Label>Field Name</Form.Label>
+                <Form.Control
+                    type="text"
+                    placeholder="Enter field name"
+                    value={fieldName}
+                    onChange={(e) => setFieldName(e.target.value)}
+                />
+              </Form.Group>
+              <br></br>
+              <Button variant="primary" type="button"onClick={ saveAnnotation }>
+                  Save Field
+              </Button>
+            </Form>
+          </div> 
         </div>
     );  
 }
