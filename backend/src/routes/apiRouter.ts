4526ac729816c38d3f24d93aadc04a9689503aab
Haris Mališević <malisevic.haris@gmail.com>
2025-04-16 01:56:32 +0200

    Route definition style standardized

--- backend/src/routes/apiRouter.ts
+++ backend/src/routes/apiRouter.ts
@@ -13,48 +13,86 @@ const FRONTEND_BUILD_PATH = path.join(__dirname, "../../../frontend/build");
 const API_ROUTER = express.Router();
 
 // Example API routes
-API_ROUTER.get("/hello", (req: Request, res: Response) => {
-    res.json({ message: "Hello from API!", requestUrl: req.url });
-});
+API_ROUTER.get(
+    "/hello",
+    (req: Request, res: Response) => {
+        res.json({ message: "Hello from API!", requestUrl: req.url });
+    }
+);
 
-API_ROUTER.get("/api/message", AuthMiddleware.isLoggedIn, AuthMiddleware.isSuperAdmin, (req, res) => {
-    const cookies = req.headers.cookie;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const jwtCookie = cookies?.split("; ").find(cookie => cookie.startsWith("jwt="))?.split("=")[1];
+API_ROUTER.get(
+    "/api/message",
+    AuthMiddleware.isLoggedIn,
+    AuthMiddleware.isSuperAdmin,
+    (req, res) => {
+        const cookies = req.headers.cookie;
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        const jwtCookie = cookies?.split("; ").find(cookie => cookie.startsWith("jwt="))?.split("=")[1];
 
-    res.json({ message: "Hello from backend!" });
-});
+        res.json({ message: "Hello from backend!" });
+    }
+);
 
 // Serve React frontend
 API_ROUTER.use(express.static(FRONTEND_BUILD_PATH));
 
-API_ROUTER.get("/", (req, res) => {
-    res.sendFile(path.join(FRONTEND_BUILD_PATH, "index.html"));
-});
+API_ROUTER.get(
+    "/",
+    (req, res) => {
+        res.sendFile(path.join(FRONTEND_BUILD_PATH, "index.html"));
+    }
+);
 
 // Auth Routes
-API_ROUTER.use("/auth", authRoutes);
+API_ROUTER.use(
+    "/auth",
+    authRoutes
+);
 
 // Route to check if the user is logged in
-API_ROUTER.get("/api/auth/status", AuthMiddleware.isLoggedIn, (req, res) => {
-    res.json({ loggedIn: true, user: req.user });
-});
+API_ROUTER.get(
+    "/api/auth/status",
+    AuthMiddleware.isLoggedIn,
+    (req, res) => {
+
+        res.json({ loggedIn: true, user: req.user });
+    }
+);
 
 // Route to check if logged in user is super admin
-API_ROUTER.get("/api/auth/status/super", AuthMiddleware.isLoggedIn, AuthMiddleware.isSuperAdmin, (req, res) => {
-    console.log("User is super admin:", req.user);
-    res.json({ loggedIn: true, user: req.user });
-});
+API_ROUTER.get(
+    "/api/auth/status/super",
+    AuthMiddleware.isLoggedIn,
+    AuthMiddleware.isSuperAdmin,
+    (req, res) => {
+        console.log("User is super admin:", req.user);
+        res.json({ loggedIn: true, user: req.user });
+    }
+);
 
 
 // API Routes
-API_ROUTER.use("/api/document-types", documentTypeRoutes);
-API_ROUTER.use("/api/sso-providers", ssoProvidersRoutes);
-API_ROUTER.use("/api/document-layouts", documentLayoutRoutes)
+API_ROUTER.use(
+    "/api/document-types",
+    documentTypeRoutes
+);
+
+API_ROUTER.use(
+    "/api/sso-providers",
+    ssoProvidersRoutes
+);
+
+API_ROUTER.use(
+    "/api/document-layouts",
+    documentLayoutRoutes
+);
 
 // Serve React frontend for any unknown routes - THIS MUST BE LAST IN THE FILE
-API_ROUTER.get("*", (req, res) => {
-    res.sendFile(path.join(FRONTEND_BUILD_PATH, "index.html"));
-});
+API_ROUTER.get(
+    "*",
+    (req, res) => {
+        res.sendFile(path.join(FRONTEND_BUILD_PATH, "index.html"));
+    }
+);
 
 export default API_ROUTER;
--- backend/src/routes/documentLayout.routes.ts
+++ backend/src/routes/documentLayout.routes.ts
@@ -4,8 +4,20 @@ import AuthMiddleware from "../middleware/AuthMiddleware";
 
 const router = Router();
 
-router.get("/", AuthMiddleware.isLoggedIn, DocumentLayoutController.getAll);
-router.post("/", AuthMiddleware.isLoggedIn, DocumentLayoutController.create);
-router.delete("/:id", AuthMiddleware.isLoggedIn, DocumentLayoutController.delete);
+router.get(
+    "/",
+    AuthMiddleware.isLoggedIn,
+    DocumentLayoutController.getAll
+);
+router.post(
+    "/",
+    AuthMiddleware.isLoggedIn,
+    DocumentLayoutController.create
+);
+router.delete(
+    "/:id",
+    AuthMiddleware.isLoggedIn,
+    DocumentLayoutController.delete
+);
 
 export default router;
